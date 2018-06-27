5ec3f635743fc09c25b4e9d05cdacb936170831c
hayato maeda <h.maeda1711@morijyobi.ac.jp>
2018-06-27 11:29:50 +0900

    実験したお

--- src/servlet/TopShow.java
+++ src/servlet/TopShow.java
@@ -27,7 +27,7 @@ public class TopShow extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stubｄｊｔｒｄｃｊ
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
