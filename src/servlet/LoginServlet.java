25f6c2a6d159e0fbd6734087d44c309433f5505e
hayato maeda <h.maeda1711@morijyobi.ac.jp>
2018-07-09 15:59:21 +0900

    ログイン画面作成(途中)

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -7,6 +7,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.StudentDAO;
+import dto.Student;
 
 /**
  * Servlet implementation class LoginServlet
@@ -27,7 +31,30 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String view = null;
+		HttpSession s = request.getSession();
+		Student result = null;
 
+		try{
+			int id = Integer.parseInt(request.getParameter("id"));
+			String pass = request.getParameter("pass");
+			if(id > 1000000){
+				result=StudentDAO.loginDAO(id, pass);
+				if(result!=null){
+					view = "/WEB^INF/view/resultLogin.jsp";
+				}else{
+					view = "/WEB-INF/view/top.jsp";
+				}
+			}
+			int id1 = result.getId();
+			String pass1 = result.getPassword();
+			s.setAttribute("id", id1);
+			s.setAttribute("pass", pass1);
+		}catch(NumberFormatException e){
+			view = "WEB-INF/veiw/top.jsp";
+
+		}
 	}
 
 	/**
